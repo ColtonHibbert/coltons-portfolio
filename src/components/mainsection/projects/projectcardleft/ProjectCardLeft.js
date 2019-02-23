4b59a1189d503889f6e4c46e8b31238e6c53a60c
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-02-22 17:49:19 -0700

    continuing card development

--- src/components/mainsection/projects/projectcardleft/ProjectCardLeft.js
+++ src/components/mainsection/projects/projectcardleft/ProjectCardLeft.js
@@ -11,16 +11,19 @@ const ProjectCardLeft= (props) => {
         w-50-l vh-50-l flex-l flex-row-l            
         ma2">
             <div className="
-            w-100 h5 flex flex-column    
-            w-40-ns h-100-ns flex-ns flex-column-ns 
-            w-50-l h-100-l flex-l flex-column-l
-            bg-white">
-                text
+            w-100 h5 flex flex-column items-center   
+            w-40-ns h-100-ns flex-ns flex-column-ns items-center-ns
+            w-40-l h-100-l flex-l flex-column-l items-center-l
+            bg-light-blue white">
+                <p>title</p>
+                <p>description</p>
+                <p>built with</p>
+                <a class="f6 link dim br3 ph3 pv2 mb2 dib white bg-dark-blue" href="#0">Button Text</a>
             </div>
             <div className="
-            w-100 h-100 flex flex-column 
-            w-60-ns height-100-ns flex-ns
-            w-60-l height-100-l flex-l
+            w-100 h-100 flex flex-column items-center
+            w-60-ns height-100-ns flex-ns items-center-ns
+            w-60-l height-100-l flex-l items-center-l
             bg-green br3">
                 img stuff
             </div>
