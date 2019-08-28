29052ffb791dbf6d03c9aabe212bc406dc366538
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-08-28 11:56:32 -0600

    Updates

--- precache-manifest.3b39b9e63c367533149184509c5b4163.js
+++ precache-manifest.3b39b9e63c367533149184509c5b4163.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "f6a08dd641fbad04391756f154c8e00d",
-    "url": "/coltons-portfolio/static/media/jillsoffice.f6a08dd6.PNG"
+    "revision": "0a38f716e119c0156ad15f5cb51e18bf",
+    "url": "/coltons-portfolio/static/media/Colton-Salt-Flats.0a38f716.jpg"
   },
   {
-    "revision": "9ae6ee2b35f0a051d074",
-    "url": "/coltons-portfolio/static/css/main.5c170cec.chunk.css"
+    "revision": "429f9e11075daf313b39",
+    "url": "/coltons-portfolio/static/css/main.46656b2e.chunk.css"
   },
   {
     "revision": "a2d295eb67a1d408f47c",
@@ -16,16 +16,20 @@ self.__precacheManifest = [
     "url": "/coltons-portfolio/static/media/javascriptcalculator.93782c7e.PNG"
   },
   {
-    "revision": "31dc1b1e36e4dfacaa8e",
-    "url": "/coltons-portfolio/static/js/2.31dc1b1e.chunk.js"
+    "revision": "3d4f465ed74469394cb3",
+    "url": "/coltons-portfolio/static/js/2.3d4f465e.chunk.js"
   },
   {
-    "revision": "0a38f716e119c0156ad15f5cb51e18bf",
-    "url": "/coltons-portfolio/static/media/Colton-Salt-Flats.0a38f716.jpg"
+    "revision": "c1e3e2a14c7fff96c38e08661a32136e",
+    "url": "/coltons-portfolio/static/media/coltonhibbertfullstackdeveloper.c1e3e2a1.pdf"
   },
   {
-    "revision": "9ae6ee2b35f0a051d074",
-    "url": "/coltons-portfolio/static/js/main.9ae6ee2b.chunk.js"
+    "revision": "429f9e11075daf313b39",
+    "url": "/coltons-portfolio/static/js/main.429f9e11.chunk.js"
+  },
+  {
+    "revision": "f6a08dd641fbad04391756f154c8e00d",
+    "url": "/coltons-portfolio/static/media/jillsoffice.f6a08dd6.PNG"
   },
   {
     "revision": "f7e36644331f4ae115205da212011c58",
@@ -40,11 +44,11 @@ self.__precacheManifest = [
     "url": "/coltons-portfolio/static/media/pomodoroclock.891187cc.PNG"
   },
   {
-    "revision": "31dc1b1e36e4dfacaa8e",
+    "revision": "3d4f465ed74469394cb3",
     "url": "/coltons-portfolio/static/css/2.38617d40.chunk.css"
   },
   {
-    "revision": "66d35b35ee56bf8df95f8fdd6bf3dbe3",
+    "revision": "d1938caacd63e14ad99cfe45df0e4222",
     "url": "/coltons-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/coltons-portfolio/precache-manifest.2a6fe28879f5ce95791fc745d7fbc104.js"
+  "/coltons-portfolio/precache-manifest.3b39b9e63c367533149184509c5b4163.js"
 );
 
 workbox.clientsClaim();
