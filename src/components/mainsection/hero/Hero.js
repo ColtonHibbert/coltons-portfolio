b31baae42b08c5f6b6458b13aac7569cf1ab4891
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-09-15 13:32:33 -0600

    working on img

--- src/components/mainsection/hero/Hero.js
+++ src/components/mainsection/hero/Hero.js
@@ -7,7 +7,7 @@ import './hero.css';
 const Hero = (props) => {
     return (
         <div className="w-100 flex flex-column items-center bg-white bb b--moon-gray">
-             <img src="https://github.com/ColtonHibbert/coltons-portfolio/blob/master/src/components/mainsection/hero/Colton-Salt-Flats.jpg?raw=true" alt="colton" className="hero-img pa2 mt3 br-100 ba b--moon-gray"/>     
+             <img src={img}/>     
              <h1 className="mb1 pb1">Colton Hibbert</h1>
              <h2 className="mt1 pt0">Full-Stack Web Developer</h2> 
              <h2>About</h2>
